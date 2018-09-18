16271afe4564803e4977b36dbdb20456c58c9016
Stephanie Coy <scoy028@gmail.com>
2018-09-18 21:26:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function theBeatlesPlay(musiciansArray, instumentsArray) {
   let string = "";
   for (let i = 0; i < musiciansArray.length - 1; ++i) {
     string = musiciansArray[i] + " plays the " + instumentsArray[i];
-    statementArray += string.push();
+    statementArray += string.push;
   }
 }
